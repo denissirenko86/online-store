9f95c7132cbea1fdd2de84eaefbdc898f46c7df8
Denis Sirenko <sirenko.denis86@gmail.com>
2018-06-25 21:27:42 +0300

    main

--- app/js/init.js
+++ app/js/init.js
@@ -13,4 +13,39 @@
 
     // Init jQuery form styler
     $('.select-styler, .input-file-styler').styler();
+
+    // Slick carousel
+    // $('.product-slider').slick({
+    //     dots: true,
+    //     infinite: true,
+    //     slidesToShow: 4,
+    //     slidesToScroll: 1,
+    //     arrows: false,
+    //     autoplay: true,
+    //     responsive: [
+    //       {
+    //         breakpoint: 1024,
+    //         settings: {
+    //           slidesToShow: 3,
+    //           slidesToScroll: 3,
+    //           infinite: true,
+    //           dots: false
+    //         }
+    //       },
+    //       {
+    //         breakpoint: 600,
+    //         settings: {
+    //           slidesToShow: 2,
+    //           slidesToScroll: 2
+    //         }
+    //       },
+    //       {
+    //         breakpoint: 480,
+    //         settings: {
+    //           slidesToShow: 1,
+    //           slidesToScroll: 1
+    //         }
+    //       }
+    //     ]
+    //   });
 })(jQuery);
